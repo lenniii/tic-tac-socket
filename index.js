0d4824f2d6108ee546cdfd6b762932899fcfa2f7
Lennii <dennisalessandrini@gmail.com>
2019-09-25 02:30:05 +0200

    try again

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-app.use(express.static(__dirname + "client/build"));
+app.use("/static", express.static(path.join(__dirname, "client/build")));
 
 app.get("/server/", (req, res) => {});
 
