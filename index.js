8e1e931ce1bd06ce391952015312bf46a5de1937
Lennii <dennisalessandrini@gmail.com>
2019-09-25 02:37:24 +0200

    try again

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const path = require("path");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-app.use("/static", express.static(path.join(__dirname, "client/build")));
+app.use("/static", express.static(path.join(__dirname, "/client/build")));
 
-app.get("/server/", (req, res) => {});
+app.get("/server/", (req, res) => {
+  res.sendStatus(200);
+});
 
 app.get("*", (req, res) =>
-  res.sendfile(__dirname + "/client/build/index.html")
+  res.sendFile(path.join(__dirname, "/client/build/index.html"))
 );
 
 const port = process.env.PORT || 5000;
